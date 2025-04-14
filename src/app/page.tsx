55c01709b68fd3e528d6a4068c44811a5ac6db45
Andy Vu <andyvu2026@u.northwestern.edu>
2025-04-14 15:49:43 -0500

    finished v1 of landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,28 +24,32 @@ export default function Home() {
 			<Marquee>
 				{
 					patterns.map((pattern) => (
-						<p key={pattern}>{pattern}</p>
+						<p className="text-shadow-orange" key={pattern}>{pattern}</p>
 					))
 				}
 			</Marquee>
 
-			<div className="bg-background overflow-hidden flex gap-3 flex-col justify-center items-center">
-				<div className="text-center font-bold text-4xl">
-					<h1 className="inline"> Welcome to </h1>
+			<div className="bg-background flex gap-3 flex-col justify-center items-center">
+				<div className="text-center text-shadow-orange font-bold text-4xl">
+					<h1 className="inline">Welcome to </h1>
 					<h1 className="text-theme-orange inline">LeetPatterns.ai</h1>
 				</div>
 
 				<p
-					className="sm:max-w-full md:max-w-1/2 text-center"
+					className="sm:max-w-full md:max-w-3/5 text-center"
 				>
 					Want to land your dream software engineering role? Learn how to identify common LeetCode patterns to ace your next interview!
 				</p>
+
+				<button className="hover:bg-theme-hover-orange transition-colors py-7 px-9 mt-7 font-bold text-2xl bg-theme-orange rounded-lg">
+					Get Started - It's All Free
+				</button>
 			</div>
 
 			<Marquee>
 				{
 					patterns.map((pattern) => (
-						<p>{pattern}</p>
+						<p className="text-shadow-orange">{pattern}</p>
 					))
 				}
 			</Marquee>
