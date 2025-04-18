682b73078c1ea02f015a2a0fa98923cee78b640b
Andy Vu <andyvu2026@u.northwestern.edu>
2025-04-18 00:20:52 -0500

    added hover to stat circle

--- src/components/StatCircle.tsx
+++ src/components/StatCircle.tsx
@@ -1,33 +1,34 @@
-import { PatternStat } from "@/interfaces/PatternStat";
-import { AnimatedCircularProgressBar } from "./magicui/animated-circular-progress-bar";
-import { useEffect, useState } from "react";
-import { patternColors } from "@/utils/Consts";
+import { PatternStat } from '@/interfaces/PatternStat'
+import { AnimatedCircularProgressBar } from './magicui/animated-circular-progress-bar'
+import { useEffect, useState } from 'react'
+import { patternColors } from '@/utils/Consts'
 
 interface StatCircleProps {
-    stat: PatternStat
+	stat: PatternStat
 }
 
-export default function StatCircle( { stat }: StatCircleProps) {
+export default function StatCircle({ stat }: StatCircleProps) {
+	const [value, setValue] = useState(0)
 
-    const [value, setValue] = useState(0);
- 
-    useEffect(() => {
-        setValue((stat.correct / stat.attempts) * 100)
-    }, []);
+	useEffect(() => {
+		setValue((stat.correct / stat.attempts) * 100)
+	}, [])
 
-    return (
-        <div className="flex gap-3 flex-col items-center">
-            <AnimatedCircularProgressBar
-                max={100}
-                min={0}
-                value={value}
-                gaugePrimaryColor={patternColors[stat.pattern]}
-                gaugeSecondaryColor="#363535"
-                strokeWidth={6}
-                size={"size-30"}
-            />
+	return (
+		<div className="flex gap-3 flex-col items-center">
+			<AnimatedCircularProgressBar
+				max={100}
+				min={0}
+				value={value}
+				correct={stat.correct}
+				attempts={stat.attempts}
+				gaugePrimaryColor={patternColors[stat.pattern]}
+				gaugeSecondaryColor="#363535"
+				strokeWidth={6}
+				size={'size-30'}
+			/>
 
-            <p className="font-medium">{stat.pattern}</p>
-        </div>
-    )
+			<p className="font-medium">{stat.pattern}</p>
+		</div>
+	)
 }
