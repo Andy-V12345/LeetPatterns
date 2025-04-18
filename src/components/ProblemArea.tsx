1a07550341e8221e5bcc571fbf09eca5c5a53905
Andy Vu <andyvu2026@u.northwestern.edu>
2025-04-18 00:45:15 -0500

    implemented weak patterns

--- src/components/ProblemArea.tsx
+++ src/components/ProblemArea.tsx
@@ -1,141 +1,151 @@
-import Problem from "@/interfaces/Problem"
-import ProblemCard from "./ProblemCard"
-import { useCallback, useEffect, useState } from "react"
-import { Pattern, ProblemCardState } from "@/utils/Types"
-import ProblemAnswer from "./ProblemAnswer"
-import { generateProblem } from "@/utils/GeminiFunctions"
-import RecapCard from "./RecapCard"
-import { motion, AnimatePresence } from "framer-motion"
-import { PatternStat } from "@/interfaces/PatternStat"
+import Problem from '@/interfaces/Problem'
+import ProblemCard from './ProblemCard'
+import { useCallback, useEffect, useState } from 'react'
+import { Pattern, ProblemCardState } from '@/utils/Types'
+import ProblemAnswer from './ProblemAnswer'
+import { generateProblem } from '@/utils/GeminiFunctions'
+import RecapCard from './RecapCard'
+import { motion, AnimatePresence } from 'framer-motion'
+import { PatternStat } from '@/interfaces/PatternStat'
 
 interface ProblemAreaProps {
-    focusedPatterns: string[] | undefined | null
+	focusedPatterns: string[] | undefined | null
 }
 
 export default function ProblemArea({ focusedPatterns }: ProblemAreaProps) {
+	const [cardState, setCardState] = useState<ProblemCardState>('default')
+	const [showAnswer, setShowAnswer] = useState(false)
+	const [problem, setProblem] = useState<Problem | null>()
+	const [questionCount, setQuestionCount] = useState(0)
+	const [showRecap, setShowRecap] = useState(false)
+	const [patternStats, setPatternStats] = useState<PatternStat[]>([])
 
-    const [cardState, setCardState] = useState<ProblemCardState>("default")
-    const [showAnswer, setShowAnswer] = useState(false)
-    const [problem, setProblem] = useState<Problem | null>()
-    const [questionCount, setQuestionCount] = useState(0)
-    const [showRecap, setShowRecap] = useState(false)
-    const [patternStats, setPatternStats] = useState<PatternStat[]>([])
+	const createNewProblem = useCallback(async () => {
+		if (focusedPatterns != null && !showRecap) {
+			if (questionCount > 0 && questionCount % 1 == 0) {
+				setShowRecap(true)
+			} else {
+				setCardState('loading')
 
-    const createNewProblem = useCallback(async () => {
-        if (focusedPatterns != null && !showRecap) {
-            if (questionCount > 0 && questionCount % 1 == 0) {
-                setShowRecap(true)
-            }
-            else {
-                setCardState("loading")
+				const res = await generateProblem(
+					focusedPatterns as Pattern[],
+					patternStats
+				)
 
-                const res = await generateProblem(focusedPatterns as string[])
+				setProblem(res)
+				setShowAnswer(false)
+				setQuestionCount((prev) => {
+					return prev + 1
+				})
+				setCardState('default')
+			}
+		} else if (focusedPatterns == null) {
+			console.error('focusedPatterns undefined')
+		} else if (showRecap) {
+			setShowRecap(false)
+			setCardState('loading')
 
-                setProblem(res) 
-                setShowAnswer(false)
-                setQuestionCount(prev => {
-                    return prev + 1
-                })
-                setCardState("default")
-            }
-        }
-        else if (focusedPatterns == null) {
-            console.error("focusedPatterns undefined")
-        }
-        else if (showRecap) {
-            setShowRecap(false)
-            setCardState("loading")
+			const res = await generateProblem(
+				focusedPatterns as Pattern[],
+				patternStats
+			)
 
-            const res = await generateProblem(focusedPatterns as string[])
+			setProblem(res)
+			setShowAnswer(false)
+			setQuestionCount((prev) => {
+				return prev + 1
+			})
+			setCardState('default')
+		}
+	}, [focusedPatterns, showRecap, questionCount])
 
-            setProblem(res) 
-            setShowAnswer(false)
-            setQuestionCount(prev => {
-                return prev + 1
-            })
-            setCardState("default")
-        }
-    }, [focusedPatterns, showRecap, questionCount])
+	const updatePatternStats = useCallback(
+		(pattern: Pattern, isCorrect: boolean) => {
+			setPatternStats((prevStats) => {
+				const existingIndex = prevStats.findIndex(
+					(stat) => stat.pattern === pattern
+				)
 
-    const updatePatternStats = useCallback((pattern: Pattern, isCorrect: boolean) => {
-        setPatternStats(prevStats => {
-            const existingIndex = prevStats.findIndex(stat => stat.pattern === pattern);
+				if (existingIndex !== -1) {
+					const updated = [...prevStats]
+					const stat = updated[existingIndex]
+					updated[existingIndex] = {
+						...stat,
+						correct: stat.correct + (isCorrect ? 1 : 0),
+						attempts: stat.attempts + 1,
+					}
 
-            if (existingIndex !== -1) {
-                const updated = [...prevStats];
-                const stat = updated[existingIndex];
-                updated[existingIndex] = {
-                    ...stat,
-                    correct: stat.correct + (isCorrect ? 1 : 0),
-                    attempts: stat.attempts + 1,
-                };
+					return updated
+				} else {
+					return [
+						...prevStats,
+						{
+							pattern,
+							correct: isCorrect ? 1 : 0,
+							attempts: 1,
+						},
+					]
+				}
+			})
+		},
+		[]
+	)
 
-                return updated;
-            } else {
-                return [
-                    ...prevStats,
-                    {
-                        pattern,
-                        correct: isCorrect ? 1 : 0,
-                        attempts: 1,
-                    },
-                ];
-            }
-        });
-    }, []);
-
-    useEffect(() => {
+	useEffect(() => {
 		if (focusedPatterns != null) {
 			createNewProblem()
 		}
 	}, [focusedPatterns])
 
-    return (
-        <div className="flex gap-5 self-stretch h-4/5 max-h-[600px] relative">
-            <AnimatePresence mode="wait">
-                {showRecap && (
-                    <motion.div
-                        key="recap"
-                        initial={{ x: "100%", opacity: 0 }}
-                        animate={{ x: 0, opacity: 1 }}
-                        exit={{ x: "-100%", opacity: 0 }}
-                        transition={{ duration: 0.4 }}
-                        className="w-full h-full"
-                    >
-                        <RecapCard patternStats={patternStats} createNewProblem={createNewProblem} />
-                    </motion.div>
-                )}
+	return (
+		<div className="flex gap-5 self-stretch h-4/5 max-h-[600px] relative">
+			<AnimatePresence mode="wait">
+				{showRecap && (
+					<motion.div
+						key="recap"
+						initial={{ x: '100%', opacity: 0 }}
+						animate={{ x: 0, opacity: 1 }}
+						exit={{ x: '-100%', opacity: 0 }}
+						transition={{ duration: 0.4 }}
+						className="w-full h-full"
+					>
+						<RecapCard
+							patternStats={patternStats}
+							createNewProblem={createNewProblem}
+						/>
+					</motion.div>
+				)}
 
-                {!showRecap && (
-                    <motion.div
-                        key="problem"
-                        initial={{ x: "100%", opacity: 1 }}
-                        animate={{ x: 0, opacity: 1 }}
-                        exit={{ x: "-100%", opacity: 0 }}
-                        transition={{ duration: 0.4 }}
-                        className="flex max-h-full self-stretch w-full gap-5"
-                    >
-                        {(problem != null || cardState === "loading") && (
-                            <>
-                                <ProblemCard
-                                    problem={problem}
-                                    cardState={cardState}
-                                    setCardState={setCardState}
-                                    showAnswer={showAnswer}
-                                    setShowAnswer={setShowAnswer}
-                                    updatePatternStats={updatePatternStats}
-                                />
-                                <ProblemAnswer
-                                    showAnswer={showAnswer}
-                                    cardState={cardState}
-                                    answer={problem?.answer ?? null}
-                                    createNewProblem={createNewProblem}
-                                />
-                            </>
-                        )}
-                    </motion.div>
-                )}
-            </AnimatePresence>
-        </div>
-    )
+				{!showRecap && (
+					<motion.div
+						key="problem"
+						initial={{ x: '100%', opacity: 1 }}
+						animate={{ x: 0, opacity: 1 }}
+						exit={{ x: '-100%', opacity: 0 }}
+						transition={{ duration: 0.4 }}
+						className="flex max-h-full self-stretch w-full gap-5"
+					>
+						{(problem != null || cardState === 'loading') && (
+							<>
+								<ProblemCard
+									problem={problem}
+									cardState={cardState}
+									setCardState={setCardState}
+									showAnswer={showAnswer}
+									setShowAnswer={setShowAnswer}
+									updatePatternStats={updatePatternStats}
+								/>
+								<ProblemAnswer
+									showAnswer={showAnswer}
+									cardState={cardState}
+									answer={problem?.answer ?? null}
+									createNewProblem={createNewProblem}
+								/>
+							</>
+						)}
+					</motion.div>
+				)}
+			</AnimatePresence>
+		</div>
+	)
 }
--- src/utils/GeminiFunctions.ts
+++ src/utils/GeminiFunctions.ts
@@ -1,94 +1,128 @@
-"use server"
+'use server'
 
-import { GoogleGenAI, Type } from "@google/genai";
-import { gemini_system_intructions, leetcode_practice_problems, patterns } from "./Consts";
-import Problem from "@/interfaces/Problem";
-import { shuffle } from "./UtilFunctions";
-import { LeetcodeSample } from "@/interfaces/LeetcodeSample";
-import { Pattern } from "./Types";
+import { GoogleGenAI, Type } from '@google/genai'
+import {
+	gemini_system_intructions,
+	leetcode_practice_problems,
+	patterns,
+} from './Consts'
+import Problem from '@/interfaces/Problem'
+import { shuffle } from './UtilFunctions'
+import { LeetcodeSample } from '@/interfaces/LeetcodeSample'
+import { Pattern } from './Types'
+import { PatternStat } from '@/interfaces/PatternStat'
 
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY })
 
 function generateRandomNum(max: number): number {
-    return Math.floor(Math.random() * max)
+	return Math.floor(Math.random() * max)
 }
 
-export async function generateProblem(focusedPatterns: string[]): Promise<Problem> {
-    const randomNum = generateRandomNum(2)
-    const useFocused = (randomNum < 1)
-    let pattern: string;
+// helper function to select a pattern to generate a problem for
+function selectPattern(
+	focusedPatterns: Pattern[],
+	weakPatterns: Pattern[]
+): Pattern {
+	const randomNum = generateRandomNum(4)
 
-    if (useFocused && focusedPatterns.length > 0) {
-        pattern = focusedPatterns[generateRandomNum(focusedPatterns.length)];
-    }
-    else {
-        pattern = patterns[generateRandomNum(patterns.length)]
-    }
+	if (randomNum == 0) {
+		if (focusedPatterns.length > 0) {
+			return focusedPatterns[generateRandomNum(focusedPatterns.length)]
+		}
 
-    const response = await ai.models.generateContent({
-        model: "gemini-2.0-flash",
-        contents: `Generate a medium-difficulty LeetCode-style problem that uses the ${pattern} pattern.`,
-        config: {
-            responseMimeType: "application/json",
-            responseSchema: {
-                type: Type.OBJECT,
-                properties: {
-                    problem: {
-                        type: Type.STRING,
-                        description: "A medium-difficulty LeetCode-style problem prompt (3–5 sentences)."
-                    },
-                    correctPattern: {
-                        type: Type.STRING,
-                        description: "One of the known pattern names."
-                    },
-                    explanation: {
-                        type: Type.STRING,
-                        description: "Explanation on why this pattern applies and how it is used in a solution."
-                    },
-                    distractorPatterns: {
-                        type: Type.ARRAY,
-                        description: "An array of 3 plausible but incorrect pattern names.",
-                        items: {
-                            type: Type.STRING
-                        },
-                        minItems: "3",
-                        maxItems: "3"
-                    }
-                },
-                required: [
-                    "problem",
-                    "correctPattern",
-                    "explanation",
-                    "distractorPatterns"
-                ],
-            },
-            systemInstruction: gemini_system_intructions,
-        },
-    });
+		return patterns[generateRandomNum(patterns.length)]
+	} else if (randomNum == 1) {
+		if (weakPatterns.length > 0) {
+			return weakPatterns[generateRandomNum(weakPatterns.length)]
+		}
 
-    const resText = response.text
-    const resObj = JSON.parse(resText as string)
+		return patterns[generateRandomNum(patterns.length)]
+	} else {
+		return patterns[generateRandomNum(patterns.length)]
+	}
+}
 
-    let options: string[] = [...resObj.distractorPatterns, resObj.correctPattern]
-    options = shuffle(options)
+export async function generateProblem(
+	focusedPatterns: Pattern[],
+	patternStats: PatternStat[]
+): Promise<Problem> {
+	// any pattern with a less than 40% accuracy is weak
+	const weakPatterns: Pattern[] = patternStats
+		.filter((stat) => stat.correct / stat.attempts < 0.4)
+		.map((stat) => stat.pattern)
 
-    const leetcode_sample = getLeetcodeSample(resObj.correctPattern)
+	const pattern = selectPattern(focusedPatterns, weakPatterns)
 
-    const problem: Problem = {
-        prompt: resObj.problem,
-        options: options as Pattern[],
-        answer: {
-            correct: resObj.correctPattern,
-            explanation: resObj.explanation,
-            leetcodeUrl: leetcode_sample.url,
-            leetcodeTitle: leetcode_sample.title
-        }
-    }
+	const response = await ai.models.generateContent({
+		model: 'gemini-2.0-flash',
+		contents: `Generate a medium-difficulty LeetCode-style problem that uses the ${pattern} pattern.`,
+		config: {
+			responseMimeType: 'application/json',
+			responseSchema: {
+				type: Type.OBJECT,
+				properties: {
+					problem: {
+						type: Type.STRING,
+						description:
+							'A medium-difficulty LeetCode-style problem prompt (3–5 sentences).',
+					},
+					correctPattern: {
+						type: Type.STRING,
+						description: 'One of the known pattern names.',
+					},
+					explanation: {
+						type: Type.STRING,
+						description:
+							'Explanation on why this pattern applies and how it is used in a solution.',
+					},
+					distractorPatterns: {
+						type: Type.ARRAY,
+						description:
+							'An array of 3 plausible but incorrect pattern names.',
+						items: {
+							type: Type.STRING,
+						},
+						minItems: '3',
+						maxItems: '3',
+					},
+				},
+				required: [
+					'problem',
+					'correctPattern',
+					'explanation',
+					'distractorPatterns',
+				],
+			},
+			systemInstruction: gemini_system_intructions,
+		},
+	})
 
-    return problem
+	const resText = response.text
+	const resObj = JSON.parse(resText as string)
+
+	let options: string[] = [
+		...resObj.distractorPatterns,
+		resObj.correctPattern,
+	]
+	options = shuffle(options)
+
+	const leetcode_sample = getLeetcodeSample(resObj.correctPattern)
+
+	const problem: Problem = {
+		prompt: resObj.problem,
+		options: options as Pattern[],
+		answer: {
+			correct: resObj.correctPattern,
+			explanation: resObj.explanation,
+			leetcodeUrl: leetcode_sample.url,
+			leetcodeTitle: leetcode_sample.title,
+		},
+	}
+
+	return problem
 }
 
 function getLeetcodeSample(pattern: string): LeetcodeSample {
-    const leetcode_samples = leetcode_practice_problems[pattern];
-    return leetcode_samples[generateRandomNum(leetcode_samples.length)]
+	const leetcode_samples = leetcode_practice_problems[pattern]
+	return leetcode_samples[generateRandomNum(leetcode_samples.length)]
 }
