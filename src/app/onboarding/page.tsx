19f77bd66fa9ad3c8a24d747daad4e41a4f347aa
Andy Vu <andyvu2026@u.northwestern.edu>
2025-04-25 15:47:13 -0500

    mobile for onboarding

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useAuth, useProtectedRoute } from '@/components/AuthContext'
+import { useIsMobile } from '@/hooks/use-mobile'
 import { patterns } from '@/utils/Consts'
 import { UIState } from '@/utils/Types'
 import { redirect } from 'next/navigation'
@@ -26,34 +27,40 @@ export default function OnboardingPage() {
 		}
 	}
 
+	const isMobile = useIsMobile()
+
 	return (
 		<div className="bg-background overflow-hidden h-[100vh] flex flex-col justify-center items-center px-5">
-			<div className="h-3/5 sm:w-4/5 md:w-3/5 xl:w-2/5 bg-card-bg overflow-y-scroll flex flex-col justify-between gap-5 rounded-xl p-8">
-				<h1 className="font-bold text-2xl">
-					Any patterns to focus on?
-				</h1>
+			<div
+				className={`h-fit w-full sm:w-4/5 md:w-3/5 xl:w-2/5 overflow-y-scroll flex flex-col gap-20 rounded-xl ${isMobile ? '' : 'p-8 bg-card-bg'}`}
+			>
+				<div className="flex flex-col gap-5">
+					<h1 className="font-bold text-2xl">
+						Any patterns to focus on?
+					</h1>
 
-				<div className="flex flex-wrap gap-3 gap-y-3">
-					{patterns.map((pattern) => (
-						<button
-							disabled={uiState == 'loading'}
-							onClick={() => {
-								const tmp = { ...patternsSelected }
-								tmp[pattern] = !tmp[pattern]
-								setPatternsSelected(tmp)
-							}}
-							key={pattern}
-							className={`${patternsSelected[pattern] ? 'bg-theme-orange hover:bg-theme-hover-orange' : 'bg-transparent hover:opacity-75'} font-medium transition-all text-sm border-theme-orange border-1 py-2 px-4 rounded-full`}
-						>
-							{pattern}
-						</button>
-					))}
+					<div className="flex flex-wrap gap-3 gap-y-3">
+						{patterns.map((pattern) => (
+							<button
+								disabled={uiState == 'loading'}
+								onClick={() => {
+									const tmp = { ...patternsSelected }
+									tmp[pattern] = !tmp[pattern]
+									setPatternsSelected(tmp)
+								}}
+								key={pattern}
+								className={`${patternsSelected[pattern] ? 'bg-theme-orange hover:bg-theme-hover-orange' : 'bg-transparent hover:opacity-75'} font-medium transition-all text-sm border-theme-orange border-1 py-2 px-4 rounded-full`}
+							>
+								{pattern}
+							</button>
+						))}
+					</div>
 				</div>
 
 				<button
 					disabled={uiState == 'loading'}
 					onClick={handleContinue}
-					className={`self-end mt-auto font-medium bg-theme-orange ${uiState == 'loading' ? '' : 'hover:bg-theme-hover-orange'} transition-colors px-5 py-3 text-base rounded-4xl`}
+					className={`self-end font-medium bg-theme-orange ${uiState == 'loading' ? '' : 'hover:bg-theme-hover-orange'} transition-colors px-5 py-3 text-base rounded-4xl`}
 				>
 					{uiState == 'loading' ? (
 						<BeatLoader
